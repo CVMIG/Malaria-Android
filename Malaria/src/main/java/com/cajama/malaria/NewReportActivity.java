7beed8b0410e62e343871313fee054ad69076074
marven <marven.sanchez@gmail.com>
2013-07-27 00:32:07 +0800

    made thumbnails load lower resolution images to reduce memory cost

--- Malaria/src/main/java/com/cajama/malaria/NewReportActivity.java
+++ Malaria/src/main/java/com/cajama/malaria/NewReportActivity.java
@@ -240,11 +240,39 @@ public class NewReportActivity extends SherlockActivity {
         // Another interface callback
     }
 
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
             BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
+            bmpFactoryOptions.inJustDecodeBounds = true;
 
             Bitmap bmp = BitmapFactory.decodeFile(imageFilePath, bmpFactoryOptions);
+
+            bmpFactoryOptions.inSampleSize = calculateInSampleSize(bmpFactoryOptions, 100, 100);
+            bmpFactoryOptions.inJustDecodeBounds = false;
+
+            bmp = BitmapFactory.decodeFile(imageFilePath, bmpFactoryOptions);
 
             myBitmap bmpp = new myBitmap();
             bmpp.image = bmp;
